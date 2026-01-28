6bc0dc81e49217eaef5d6a8de8ab501e45b9142f
Kimberly Dauber <noteflightthemes@gmail.com>
2026-01-27 21:29:22 -0500

    Don't scroll the whole page while the mobile nav is open

--- assets/js/mobile-nav-dialog.js
+++ assets/js/mobile-nav-dialog.js
@@ -2,19 +2,24 @@ const dialog = document.getElementById("mobileNavDialog");
 const openButton = document.getElementById("mobileNavDialogOpenButton");
 const closeButton = document.getElementById("closeMobileNavDialogButton");
 
+const html = document.querySelector("html");
+
 // "Show the dialog" button opens the dialog modally
 openButton.addEventListener("click", () => {
   dialog.showModal();
+  html.style.overflow = "hidden";
 });
 
 // "Close" button closes the dialog
 closeButton.addEventListener("click", () => {
   dialog.close();
+  html.style.overflow = "unset";
 });
 
 // Close the dialog if the window is resized to not need the mobile nav
 addEventListener("resize", () => {
   if (window.innerWidth >= 850 && dialog.open) {
     dialog.close();
+    html.style.overflow = "unset";
   }
 });
